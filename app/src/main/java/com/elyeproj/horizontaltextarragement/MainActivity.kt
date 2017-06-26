8e3688566014b677fa55f18818863ad6b285a014
yombunk3r <jorgecool@gmail.com>
2017-06-26 06:32:31 -0700

    - Adding another way of getting the same result:   - Constraint Layout

--- app/src/main/java/com/elyeproj/horizontaltextarragement/MainActivity.kt
+++ app/src/main/java/com/elyeproj/horizontaltextarragement/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.elyeproj.horizontaltextarragement
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.layout_constraint_space.*
 import kotlinx.android.synthetic.main.layout_equal_space.*
 import kotlinx.android.synthetic.main.layout_wrap_space.*
 
@@ -29,6 +31,13 @@ class MainActivity : AppCompatActivity() {
                 txt_worst_wrap.visibility = View.VISIBLE
                 space_worst_wrap.visibility = View.VISIBLE
             }
+        }
+
+        btn_constraint_remove.setOnClickListener {
+            val isVisible = txt_worst_constraint.visibility == View.VISIBLE
+            TransitionManager.beginDelayedTransition(constraint_layout)
+            txt_worst_constraint.visibility =   if (isVisible) View.GONE
+                                                else View.VISIBLE
         }
     }
 }
